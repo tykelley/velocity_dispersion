9f4e555f58ebca6532b32e0caec346d1867e519e
tykelley <tyler.kelley07@gmail.com>
2018-12-04 17:48:52 -0600

    Overall pep8 cleanup

--- dispersion.py
+++ dispersion.py
@@ -3,7 +3,7 @@ from glob import glob
 import os.path
 
 import matplotlib as mpl
-mpl.use("Agg")
+mpl.use("Agg")  # noqa
 from matplotlib import pyplot as plt
 
 from helpers import analyze_halo
@@ -27,40 +27,41 @@ parser.add_argument('--rmax', dest="rmax", type=float,
 parser.add_argument('--binsize', dest="bin_size", type=float,
                     help="Size of radial bins. [%(default)s kpc]",
                     default=1)
-parser.add_argument('--pandas', dest="pandas", action='store_true', 
+parser.add_argument('--pandas', dest="pandas", action='store_true',
                     help="Use pandas for shell calculations. [%(default)s]",
                     default=False)
 args = parser.parse_args()
 
-ptype_map = {0 : 'PartType0',
-             1 : 'PartType1',
-             2 : 'PartType2',
-             4 : 'PartType4',
+ptype_map = {0: 'PartType0',
+             1: 'PartType1',
+             2: 'PartType2',
+             4: 'PartType4',
              }
 p_type = ptype_map[args.ptype]
 
-fig,ax = plt.subplots(figsize=(12,12))
+fig, ax = plt.subplots(figsize=(12, 12))
 mpl.rc('axes', linewidth=3)
-plt.yticks(fontsize = 10)
-plt.xticks(fontsize = 10)
+plt.yticks(fontsize=10)
+plt.xticks(fontsize=10)
 plt.tick_params(which='minor', width=2, length=10)
 plt.tick_params(which='major', width=2, length=15)
 
-for run_type in args.subdirs:
+for run_type in args.subdirs.split(','):
     root_path = os.path.join(args.inbase, run_type)
     particle_file = os.path.join(root_path, 'snapshot_' + str(args.snap_num))
     halo_file = os.path.join(root_path, 'halo_' + str(args.snap_num))
     num_of_file = len(glob(particle_file + '*'))
-    x,y = analyze_halo(particle_file, halo_file, p_type, num_of_file, 
+    x, y = analyze_halo(particle_file, halo_file, p_type, num_of_file,
                         rmax=args.rmax, bin_size=args.bin_size,
                         use_pandas=args.pandas)
     ax.semilogx(x, y, linewidth=4.5, color='red', label=run_type)
 
 plt.legend(loc=0)
 plt.axis([0, 400, 0, 420])
-plt.title('Velocity Dispersion Comparison: M12i dark vs. M12i', fontsize = 25)
-plt.xlabel(r'$Radius\, [kpc]$', fontsize = 30)
-plt.ylabel(r'$\sigma_{disp}\, [ km s^{-1}]$', fontsize = 30)
+plt.title('Velocity Dispersion Comparison: M12i dark vs. M12i', fontsize=25)
+plt.xlabel(r'$Radius\, [kpc]$', fontsize=30)
+plt.ylabel(r'$\sigma_{disp}\, [ km s^{-1}]$', fontsize=30)
 
-save_fig_file = args.outbase+'dispersion_'+str(args.snap_num)+'.png'
+fig_name = 'dispersion_' + str(args.snap_num) + '.png'
+save_fig_file = os.path.join(args.outbase, fig_name)
 fig.savefig(save_fig_file)
--- helpers.py
+++ helpers.py
@@ -6,35 +6,38 @@ import pandas as pd
 def analyze_halo(particle_file, halo_file, p_type='PartType1', num_of_file=1,
                  rmax=300, bin_size=1, use_pandas=False):
     r, v = get_radius_and_velocity(particle_file, halo_file, num_of_file,
-                                    p_type)
+                                   p_type)
     if use_pandas:
         return dispersion_in_shells_pd(r, v, rmax, bin_size)
     else:
         return dispersion_in_shells(r, v, rmax, bin_size)
 
+
 def dispersion_in_shells(radius, velocities, rmax, bin_size):
     radial_bins = np.arange(0, rmax, bin_size)
     dispersion = np.zeros(radial_bins.size - 1)
 
-    for i in range(1, len(radial_bins)): # Start @ 1 since we're making shells
-        mask = (radius >  radial_bins[i-1]) & (radius < radial_bins[i])
+    for i in range(1, len(radial_bins)):  # Start @ 1 since we're making shells
+        mask = (radius > radial_bins[i-1]) & (radius < radial_bins[i])
         v_avg = velocities[mask].mean(axis=0)
-        difference = velocities[mask] - v_avg  
+        difference = velocities[mask] - v_avg
         coord_var = np.var(difference, axis=0)
         dispersion[i-1] = np.sqrt(np.sum(coord_var))
     return radial_bins[1:], dispersion
 
+
 def dispersion_in_shells_pd(radius, velocities, rmax, bin_size):
     radial_bins = np.arange(0, rmax, bin_size)
-    df = pd.DataFrame({"r" : radius,
-                       "vx" : velocities[:,0],
-                       "vy" : velocities[:,1], 
-                       "vz" : velocities[:,2],
+    df = pd.DataFrame({"r": radius,
+                       "vx": velocities[:, 0],
+                       "vy": velocities[:, 1],
+                       "vz": velocities[:, 2],
                        })
     shells = pd.cut(df.r, radial_bins)
     groups = df.drop("r", axis="columns").groupby(shells)
     dispersion = np.sqrt(groups.var().sum(axis=1))
-    return radial_bins[1:], dispersion.values 
+    return radial_bins[1:], dispersion.values
+
 
 def get_data(filename, num_of_file, key1, key2):
     if num_of_file == 1:
@@ -44,28 +47,28 @@ def get_data(filename, num_of_file, key1, key2):
         else:
             return f[key1][key2][:]
     else:
-        for i in range(0,num_of_file):
+        for i in range(0, num_of_file):
             f = h5py.File(filename+'.'+str(i)+'.hdf5', 'r')
             if key1 == 'Header':
                 return f[key1].attrs[key2]
             else:
-                if ( len(f[key1][key2][:].shape)==1 ):
-                    if i==0:
+                if (len(f[key1][key2][:].shape) == 1):
+                    if i == 0:
                         result = f[key1][key2][:]
                     else:
-                        result = np.hstack( (result,f[key1][key2][:]) )
+                        result = np.hstack((result, f[key1][key2][:]))
                 else:
-                    if i==0:
+                    if i == 0:
                         result = f[key1][key2][:]
                     else:
-                        result = np.vstack( (result,f[key1][key2][:]) )
+                        result = np.vstack((result, f[key1][key2][:]))
         return result
 
+
 def get_radius_and_velocity(particle_file, halo_file, num_of_file, p_type):
-    h = get_data(particle_file, num_of_file,'Header','HubbleParam')
+    h = get_data(particle_file, num_of_file, 'Header', 'HubbleParam')
     coords = get_data(particle_file, num_of_file, p_type, 'Coordinates')/h
-    velocities  = get_data(particle_file, num_of_file, p_type, 'Velocities')
-    masses = get_data(particle_file, num_of_file, p_type, 'Masses')*(10**10)/h
+    velocities = get_data(particle_file, num_of_file, p_type, 'Velocities')
 
     f = h5py.File(halo_file + '.hdf5', 'r')
     halo_pos = np.array(f['position'])
