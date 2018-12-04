729b27d9662e42b0af0cd8ead26868b0a8c33dc5
tykelley <tyler.kelley07@gmail.com>
2018-12-03 22:25:39 -0600

    Updated OptsParse to ArgParse

--- dispersion.py
+++ dispersion.py
@@ -1,23 +1,28 @@
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 from helpers import analyze_halo
 
-mpl.use('Agg')
-parser = OptionParser()
-parser.add_option('--rmax', dest="rmax", type=float,
-                  help="Distance of furthest shell. Default = %default kpc",
-                  default=300)
-parser.add_option('--binsize', dest="bin_size", type=float,
-                  help="Size of radial bins. Default = %default kpc",
-                  default=1)
-parser.add_option('--pandas', dest="pandas", action='store_true', 
-                  help="Use pandas for shell calculations. Default = %default",
-                  default=False)
+parser = ArgumentParser()
+parser.add_argument('inbase', help="Path to files.")
+parser.add_argument('outbase',
+                    help="Path to save figures to.")
+parser.add_argument('snap_num', type=int,
+                    help="Snapshot number. (%(default)s)",
+                    default=1)
+parser.add_argument('--rmax', dest="rmax", type=float,
+                    help="Distance of furthest shell. (%(default)s kpc)",
+                    default=300)
+parser.add_argument('--binsize', dest="bin_size", type=float,
+                    help="Size of radial bins. (%(default)s kpc)",
+                    default=1)
+parser.add_argument('--pandas', dest="pandas", action='store_true', 
+                    help="Use pandas for shell calculations. (%(default)s)",
+                    default=False)
 
-ops,args = parser.parse_args()
+args = parser.parse_args()
 
 # Options 
 sim = 1 #... Elvis (0) or LATTE (1)
@@ -30,11 +35,11 @@ part_list = ['PartType0','PartType1','PartType2','PartType4']
 p_type = part_list[itype]
 
 if sim == 0:
-    base = '/data11/home/dmckeown/output/m12i/ELVIS/'
-    base1 = '/data11/home/dmckeown/output/m12i/ELVIS/'
+    base = '/data11/home/dmckeown/output/m12i/ELVIS'
+    base1 = '/data11/home/dmckeown/output/m12i/ELVIS'
 if sim == 1:
     base = '/data18/brunello/dmckeown/output'
-    base1 = '/data18/brunello/dmckeown/plots/'
+    base1 = '/data18/brunello/dmckeown/plots'
 
 
 fig,ax = plt.subplots(figsize=(12,12))
@@ -45,14 +50,19 @@ plt.tick_params(which='minor',width=2,length=10)
 plt.tick_params(which='major',width=2,length=15)
 
 for run_type in irun:
-    particle_file = base + '/' + run_type + '/snapshot_'+ str(isnap)
-    halo_file = base + '/' + run_type + '/halo_' + str(isnap)
+    particle_file = args.inbase + '/' + run_type + '/snapshot_'+ str(args.snap_num)
+    halo_file = arg.inbase + '/' + run_type + '/halo_' + str(args.snap_num)
 
-    x,y = analyze_halo(particle_file, halo_file, p_type, rmax=ops.rmax,
-                       bin_size=ops.binsize, use_pandas=ops.pandas)
     label = "m12i"
     if "dark" in run_type.lower():
         label += " dark"
+        num_of_file = 1
+    else:
+        num_of_file = 4
+
+    x,y = analyze_halo(particle_file, halo_file, p_type, num_of_file, 
+                        rmax=args.rmax, bin_size=args.binsize,
+                        use_pandas=args.pandas)
     ax.semilogx(x, y, linewidth=4.5, color='red', label=label)
 
 plt.legend(loc=0)
@@ -61,7 +71,5 @@ plt.title('Velocity Dispersion Comparison: M12i dark vs. M12i', fontsize = 25)
 plt.xlabel(r'$Radius\, [kpc]$', fontsize = 30)
 plt.ylabel(r'$\sigma_{disp}\, [ km s^{-1}]$', fontsize = 30)
 
-save_fig_file = base1+str(irun)+'/dm/'+str(isnap)+'.png'
-print "Saving : "
-print str(save_fig_file)
+save_fig_file = args.outbase+'dispersion_'+str(args.snap_num)+'.png'
 fig.savefig(save_fig_file)
--- helpers.py
+++ helpers.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 
 
-def analyze_halo(particle_file, halo_file, p_type=1, 
+def analyze_halo(particle_file, halo_file, p_type='PartType1', num_of_file=1,
                  rmax=300, bin_size=1, use_pandas=False):
-    r, v = get_radius_and_velocity(particle_file, halo_file, p_type)
-
+    r, v = get_radius_and_velocity(particle_file, halo_file, num_of_file,
+                                    p_type)
     if use_pandas:
         return dispersion_in_shells_pd(r, v, rmax, bin_size)
     else:
@@ -16,20 +16,16 @@ def dispersion_in_shells(radius, velocities, rmax, bin_size):
     radial_bins = np.arange(0, rmax, bin_size)
     dispersion = np.zeros(radial_bins.size - 1)
 
-    for i in range(1, len(radial_bins)): # Start @ 1 since we're making shells               
-        
+    for i in range(1, len(radial_bins)): # Start @ 1 since we're making shells
         mask = (radius >  radial_bins[i-1]) & (radius < radial_bins[i])
-
         v_avg = velocities[mask].mean(axis=0)
         difference = velocities[mask] - v_avg  
         coord_var = np.var(difference, axis=0)
         dispersion[i-1] = np.sqrt(np.sum(coord_var))
-
     return radial_bins[1:], dispersion
 
 def dispersion_in_shells_pd(radius, velocities, rmax, bin_size):
     radial_bins = np.arange(0, rmax, bin_size)
-    
     df = pd.DataFrame({"r" : radius,
                        "vx" : velocities[:,0],
                        "vy" : velocities[:,1], 
@@ -38,7 +34,6 @@ def dispersion_in_shells_pd(radius, velocities, rmax, bin_size):
     shells = pd.cut(df.radius, radial_bins)
     groups = df.groupby(shells)
     dispersion = np.sqrt(groups.var().sum(axis=1))
-
     return radial_bins[1:], dispersion.values 
 
 def get_data(filename, num_of_file, key1, key2):
@@ -66,11 +61,11 @@ def get_data(filename, num_of_file, key1, key2):
                         result = np.vstack( (result,f[key1][key2][:]) )
         return result
 
-def get_radius_and_velocity(particle_file, halo_file, p_type):
-    h = get_data(particle_file, 1,'Header','HubbleParam')
-    coords = get_data(particle_file, 1, p_type, 'Coordinates')/h
-    velocities  = get_data(particle_file, 1, p_type, 'Velocities')
-    masses = get_data(particle_file, 1, p_type, 'Masses')*(10**10)/h
+def get_radius_and_velocity(particle_file, halo_file, num_of_file, p_type):
+    h = get_data(particle_file, num_of_file,'Header','HubbleParam')
+    coords = get_data(particle_file, num_of_file, p_type, 'Coordinates')/h
+    velocities  = get_data(particle_file, num_of_file, p_type, 'Velocities')
+    masses = get_data(particle_file, num_of_file, p_type, 'Masses')*(10**10)/h
 
     f = h5py.File(halo_file + '.hdf5', 'r')
     halo_pos = np.array(f['position'])
@@ -78,7 +73,5 @@ def get_radius_and_velocity(particle_file, halo_file, p_type):
 
     idx = np.argmax(halo_mass)
     host_cent = halo_pos[idx]
-
     radius = np.linalg.norm(coords - host_cent, None, 1)
-
     return radius, velocities
